75f053ca81c4259205923877611ade1d385bbff7
Ricardo David <ricdavid.icm@hotmail.com>
2020-08-13 15:12:29 -0300

    Testando Classe de Domínio Categoria

--- src/main/java/com/ricardodavid/springio/resources/CategoriaResource.java
+++ src/main/java/com/ricardodavid/springio/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.ricardodavid.springio.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardodavid.springio.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categoria")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
