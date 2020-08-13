cbe9114288a0045f335c207afa2d993af06cdb4c
Ricardo David <ricdavid.icm@hotmail.com>
2020-08-13 14:46:26 -0300

    Projeto Criado

---
+++ src/main/java/com/ricardodavid/springio/SpringIoApplication.java
@@ -0,0 +1,13 @@
+package com.ricardodavid.springio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardodavid/springio/SpringIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardodavid.springio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
